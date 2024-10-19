91c48be1319eb21b2e6ace585d213a3c9b9d06d1
Y3ASIN <yeasinarafat.wa@gmail.com>
2024-10-19 23:25:10 +0600

    Integrating typescript with node js.

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
+
+import TodoRoute from "./routes/todos";
+
+const app = express();
+
+app.use(json());
+
+app.use("/todos", TodoRoute);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3100);
